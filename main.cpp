9c00de4caccab800f5770ba222c363a9f116ac31
Artur <vags2154@gmail.com>
2024-03-25 19:50:12 +0100

    Main program launch file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "window.h"
+#include "themes.h"
+#include <QApplication>
+
+int main(int argc, char** argv) {
+    QApplication application(argc, argv);
+
+    Window w;
+
+    QMenu* themeMenu = Themes::createThemeMenu();
+    w.menuBar()->addMenu(themeMenu);
+
+    w.setWindowTitle("База Данных: Автосалон");
+    w.show();
+
+    return application.exec();
+}
